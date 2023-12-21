99d622e9c512260309809f5a8d4fa1049f071237
Mahbubul Alam <mahbublalam500@gmail.com>
2023-12-21 20:26:32 +0600

    Nutritionist Page Complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,17 @@
-import HomePage from "./(withlayout)/home/page";
-import Cursor from "./components/Cursor/Cursor";
 import Navbar from "./components/base/navbar";
 import Footer from "./components/home/footer";
 import Reviews from "./components/home/reviews";
 import Social from "./components/home/social";
+import HomeThreePage from "./(withlayout)/home-3/page";
 
 export default function Home() {
   return (
     <>
-     
-        <Navbar />
-        <HomePage />
-        <Social />
-        <Reviews />
-        <Footer />
-      
+      <Navbar />
+      <HomeThreePage />
+      <Social />
+      <Reviews />
+      <Footer />
     </>
   );
 }
